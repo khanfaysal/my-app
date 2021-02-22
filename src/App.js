9b4844d4ddeeef02abc5f381c6b98a8bc522112f
Faysal Khan <mdfaysalkhancse@gmail.com>
2021-02-23 00:50:18 +0600

    This is my first react practice

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit done22 <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/khanfaysal"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Go My github
         </a>
       </header>
     </div>
